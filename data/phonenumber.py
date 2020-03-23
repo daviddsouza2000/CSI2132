a7980f225f4c80d53c4dd14ea6149925e623a8cd
AndrewPochapsky <andrew.pochapsky@gmail.com>
2020-03-23 14:17:01 -0400

    phonenumber done

---
+++ data/phonenumber.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import random as rand
+
+user_df = pd.read_csv("sample_users.csv")
+result_df = pd.DataFrame(columns = ["PhoneNumber", "UserId"])
+
+for i, row in user_df.iterrows():
+    num = ""
+    for x in range(9):
+        num += str(rand.randint(1, 9))
+    result_df = result_df.append({"PhoneNumber": num, "UserId": row["UserId"]}, ignore_index=True)
+result_df.to_csv("sample_phonenumbers.csv")
