7963da35be7668df0a1b04a8c0f32873ba453e4d
AndrewPochapsky <andrew.pochapsky@gmail.com>
2020-03-23 13:14:23 -0400

    started property bookking

--- data/property_booking.py
+++ data/property_booking.py
@@ -3,7 +3,13 @@ import random as rand
 
 user_df = pd.read_csv("sample_users.csv")
 property_df = pd.read_csv("sample_properties.csv")
+user_ids = list(user_df["UserId"])
 
 min_id = 1000
 max_id = 999999
 used_ids = set()
+properties = property_df.sample(frac=0.95)
+
+for prop in properties:
+
+
